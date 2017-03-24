168cf9d1bedf0f64fe50bbd6bd664417eb0bf8f5
tianzhuo <tianzhuo112@163.com>
2017-03-24 15:21:23 +0800

    Revert "auto release pool"
    This reverts commit 33ecb5c0a55fbd8d20845cd33865cce635108ef1.

--- framework/Source/GPUImageMovie.m
+++ framework/Source/GPUImageMovie.m
@@ -286,13 +286,11 @@
     {
         while (reader.status == AVAssetReaderStatusReading && (!_shouldRepeat || keepLooping))
         {
-            @autoreleasepool {
                 [weakSelf readNextVideoFrameFromOutput:readerVideoTrackOutput];
-                
-                if ( (readerAudioTrackOutput) && (!audioEncodingIsFinished) )
-                {
+
+            if ( (readerAudioTrackOutput) && (!audioEncodingIsFinished) )
+            {
                     [weakSelf readNextAudioSampleFromOutput:readerAudioTrackOutput];
-                }
             }
 
         }
